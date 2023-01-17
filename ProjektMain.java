2c4ceae26b7a12fd95caea95e4863a279b3ac190
amueller036 <a.mueller036@pm.me>
2023-01-17 09:39:23 +0100

    sad

--- ProjektMain.java
+++ ProjektMain.java
@@ -1,4 +1,8 @@
 public class ProjektMain {
     public static void main(String[] args){
+        jstadlma a = new jstadlma();
+        a.tell();
+        amueller036 b = new amueller036();
+        b.tell();
     }
 }
